373c3d82889e13f0b1c3fba96d352f6e0eb88102
Vladislav Mosin <vmos1999@mail.ru>
2017-09-16 15:43:32 +0300

    one more commit

--- string_task.py
+++ string_task.py
@@ -1,21 +1,25 @@
 def verbing(s):
-    if len(s)>2:
-        if s[-3:]=="ing":
-            s=s+"ly"
+    if len(s) > 2:
+        if s[-3:] == "ing":
+            s = s + "ly"
         else:
-            s=s+"ing"
+            s = s + "ing"
     return s
+
+
 def not_bad(s):
-    a=-1
-    b=-1
-    a=s.find("not")
-    b=s.find("bad")
-    if a<b and a!=-1:
-        s=s[:a]+"good"+s[b+3:]
+    a =- 1
+    b =- 1
+    a = s.find("not")
+    b = s.find("bad")
+    if a < b and a !=- 1:
+        s = s[:a] + "good" + s[b+3:]
     return s
+
+
 def front_back(a, b):
-    c=(len(a)+1)//2
-    d=(len(b)+1)//2
-    s=a[:c]+b[:d]+a[c:]+b[d:]
+    c = (len(a) + 1) // 2
+    d = (len(b) + 1) // 2
+    s = a[:c] + b[:d] + a[c:] + b[d:]
     return s;
 
--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,22 @@
 def di(words):
-    d=dict()
+    d = dict()
     for s in words:
-        s1=s.lower();
+        s1 = s.lower();
         if s1 not in d:
-            d[s1]=1;
+            d[s1] = 1;
         else:
-            d[s1]+=1
+            d[s1] += 1
     return d
+
+
 def print_words(words):
-    d=di(words)
-    for ukaz in sorted(d.keys(),reverse=False):
-        print(ukaz,d[ukaz])
+    d = di(words)
+    for ukaz in sorted(d.keys()):
+        print(ukaz, d[ukaz])
+
+        
 def print_top(words):
-    d=di(words)
-    d1=sorted(d.keys(),key=lambda k : d[k],reverse=True)
+    d = di(words)
+    d1 = sorted(d.keys(), key = lambda k : d[k], reverse = True)
     for uk in d1[:20]:
-        print(uk,d[uk])
+        print(uk, d[uk])
