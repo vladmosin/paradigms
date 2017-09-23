bc3529e45ecf04dc8bd64f16387789d0981745be
vladmosin <31858377+vladmosin@users.noreply.github.com>
2017-09-23 15:30:57 -0400

    Add files via upload

--- python.py
+++ python.py
@@ -4,6 +4,13 @@ import sys
 from os import path
 from hashlib import sha1 as hasher
 	
+def hashing(s):
+	with open(os.path.join(path, f_names), mode = 'rb') as f:
+		h1 = hasher()
+		s = f.read(1000)
+		if len(s) != 0:
+			n += h1.update(s).hexdigest()
+	return n
 def main():	
 	s = sys.argv[1]
 	d = dict()
@@ -11,18 +18,15 @@ def main():
 		for f_names in files:
 			if f_files[0] != '.' and f.files[0] != '~' :
 				if not os.path.islink(os.path.join(path, f_names)):
-					with open(os.path.join(path, f_names), mode = 'rb') as f:
-						h1 = hasher()
-						n = h1.update(f.read()).hexdigest()
+					n = hashing(os.path.join(path, f_names))
 					if n not in d:
-						d[n] = [1, os.path.join(path, f_names)]
+						d[n] = os.path.join(path, f_names)
 					else:
-						d[n][0] = 2
-						d[n][1].append(os.path.join(path, f_names))
+						d[n].append(os.path.join(path, f_names))
 	for hsh in d:
-		if d[hsh][0] == 2:
-			while lst in d[hsh][1]:
-				print(lst + ':')
+		if d[hsh].size() > 1:
+			while lst in d[hsh]:
+				print(lst.join(":"))
 			print('\n')
 if __name__ == "__main__":
 	main()		
