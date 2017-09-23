bdc2f94e8e0fe55fa88aa7e5dc3b29fa66bee3a7
vladmosin <31858377+vladmosin@users.noreply.github.com>
2017-09-23 17:08:24 -0400

    Add files via upload

--- python.py
+++ python.py
@@ -4,28 +4,36 @@ import sys
 from os import path
 from hashlib import sha1 as hasher
 	
-def hashing(s):
-	with open(s, mode = 'rb') as f:
-		h1 = hasher()
-		s1 = f.read(4096)
-		if len(s1) != 0:
-			n += h1.update(s1).hexdigest()
-	return n
+	
+def hashing(s1):
+	h1= hasher()
+	with open(s1, mode = 'rb') as f:
+		while True:
+			s2 = f.read(4096)
+			if not s2:
+				break
+			h1.update(s2)
+	return h1.digest()
+	
+	
 def main():	
 	s = sys.argv[1]
-	d = dict()
+	h1 = hasher()
+	way = []
 	for path, _, files in walk(s):	
 		for f in files:
 			if f[0] != '.' and f[0] != '~' and not os.path.islink(f):
-					n = hashing(os.path.join(path, f))
-					if n not in d:
-						d[n] = os.path.join(path, f)
-					else:
-						d[n].append(os.path.join(path, f))
-	for hsh in d:
-		if d[hsh].size() > 1:
-			while lst in d[hsh]:
-				print(":".join(lst))
+				way.append(os.path.join(path, f))
+	d = {}
+	for p in way:
+		k = hashing(p)
+		if k not in d:
+			d[k] = [p]
+        else:
+			d[k].append(p)
+	for hsh in d.values():
+		if len(hsh) > 1:
+			print(":".join(hsh))
 if __name__ == "__main__":
 	main()		
 				
