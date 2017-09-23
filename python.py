a13145e05883f5515d3de6196cc8c291c602df90
vladmosin <31858377+vladmosin@users.noreply.github.com>
2017-09-23 15:09:32 -0400

    Add files via upload

--- python.py
+++ python.py
@@ -12,18 +12,20 @@ def main():
 			if f_files[0] != '.' and f.files[0] != '~' :
 				if not os.path.islink(os.path.join(path, f_names)):
 					with open(os.path.join(path, f_names), mode = 'rb') as f:
-					hasher = hasher()
-					n = hasher.update(f.read()).hexdigest()
+						h1 = hasher()
+						n = h1.update(f.read()).hexdigest()
 					if n not in d:
-						d[n] = [1, path + '/' + f_names]
+						d[n] = [1, os.path.join(path, f_names)]
 					else:
 						d[n][0] = 2
-						d[n][1] += ":" + path + '/' + f_names
+						d[n][1].append(os.path.join(path, f_names))
 	for hsh in d:
 		if d[hsh][0] == 2:
-			print(d[hsh][1] + '\n')
-if '__name__' == __main__
-				
+			while lst in d[hsh][1]:
+				print(lst + ':')
+			print('\n')
+if __name__ == "__main__":
+	main()		
 				
 			
 		
