136226620de151ecbfa7cd72510bd9ce7c18d8b2
vladmosin <31858377+vladmosin@users.noreply.github.com>
2017-10-23 07:52:57 -0400

    Add files via upload

--- lab5/folder.py
+++ lab5/folder.py
@@ -25,26 +25,28 @@ class ConstantFolder:
         lhs = binary_operation.lhs.accept(self)
         rhs = binary_operation.rhs.accept(self)
         if type(lhs) == m.Number and type(rhs) == m.Number:
-            return binary_operation.evaluate(self.scope)
-        if type(binary_operation.rhs) == m.Number and\
-           type(binary_operation.lhs) == m.Reference and\
+            return m.BinaryOperation(lhs, binary_operation.op,
+                                     rhs).evaluate(self.scope)
+        if type(rhs) == m.Number and\
+           type(lhs) == m.Reference and\
            binary_operation.op == "*":
-            if binary_operation.rhs.value == 0:
+            if rhs.value == 0:
                 binary_operation.lhs = m.Number(0)
                 return m.Number(0)
-        if type(binary_operation.lhs) == m.Number and\
-           type(binary_operation.rhs) == m.Reference and\
+        if type(lhs) == m.Number and\
+           type(rhs) == m.Reference and\
            binary_operation.op == "*":
-            if binary_operation.rhs.value == 0:
+            if lhs.value == 0:
                 binary_operation.rhs = m.Number(0)
                 return m.Number(0)
-        if type(binary_operation.lhs) == m.Reference and\
-           type(binary_operation.rhs) == m.Reference and\
-           binary_operation.op == "-":
-            if binary_operation.lhs.name == binary_operation.rhs.name:
-                binary_operation = m.Number(0)
+        if type(lhs) == m.Reference and\
+           type(rhs) == m.Reference and\
+           binary_operation.op == '-':
+            if lhs.name == rhs.name:
+                binary_operation.lhs = m.Number(0)
+                binary_operation.rhs = m.Number(0)
                 return m.Number(0)
-        return binary_operation
+        return m.BinaryOperation(lhs, binary_operation.op, rhs)
 
     def visitFunctionDefinition(self, func_def):
         body = []
@@ -79,42 +81,6 @@ class ConstantFolder:
 def main():
     v = ConstantFolder()
     p = printer.PrettyPrinter()
-    p.visit(v.visit(m.Print(m.Number(10))))
-    p.visit(v.visit(m.UnaryOperation('-', m.Number(10))))
-    p.visit(v.visit(m.BinaryOperation(m.BinaryOperation(
-                              m.Reference('foo'), '+',
-                              m.Number(7)), '*', m.Number(2))))
-    p.visit(v.visit(m.Read("read")))
-    p.visit(v.visit(m.Conditional(m.Number(5), [
-                    m.BinaryOperation(m.Reference('var'), '-',
-                                      m.Number(-5))])))
-    p.visit(v.visit(m.FunctionDefinition('summer', m.Function(['a', 'b'], [
-            m.Print(m.BinaryOperation(m.Reference('a'), '+',
-                                      m.Reference('b'))),
-            m.BinaryOperation(m.Reference('a'), '+', m.Reference('b'))]))))
-    p.visit(v.visit(m.FunctionCall(m.Reference('summer'), [
-                    m.Number(1),
-                    m.BinaryOperation(m.Number(2), '+', m.Number(3))])))
-    p.visit(v.visit(m.Print(m.Conditional(m.BinaryOperation(
-          m.Number(4), '-', m.Number(4)), [], [
-          m.BinaryOperation(m.Number(9), '/', m.Number(3)), m.Reference('var')
-          ]))))
-    p.visit(v.visit(m.FunctionDefinition('abs', m.Function(['a', 'b'], [
-        m.Conditional(m.BinaryOperation(
-                      m.BinaryOperation(m.Reference('a'), '-',
-                                        m.Reference('b')), '>', m.Number(0)), [
-                    m.Print(m.BinaryOperation(m.Reference('a'), '-',
-                                              m.Reference('b')))
-                    ], [
-            m.Print(m.BinaryOperation(m.Reference('b'), '-', m.Reference('a')))
-        ])
-    ]))))
-    p.visit(v.visit(m.FunctionCall(m.Reference('abs'), [
-        m.Number(23),
-        m.UnaryOperation('-', m.Number(-30))
-    ])))
-    p.visit(v.visit(m.FunctionDefinition('fu', m.Function([], []))))
-    p.visit(v.visit(m.FunctionCall(m.Reference('fu'), [])))
 
 if __name__ == "__main__":
     main()
