e27eeb02b63b4427dc7caa361f703f2df734f385
vladmosin <31858377+vladmosin@users.noreply.github.com>
2017-12-03 04:25:37 -0500

    Add files via upload

--- fp1.hs
+++ fp1.hs
@@ -1,25 +1,25 @@
 head' (x:_) = x
 
 tail' (_:xs) = xs
-tail' _ = []
+tail' _      = []
 
-take' 0 _ = []
-take' _ [] = []
+take' 0 _      = []
+take' _ []     = []
 take' n (x:xs) = x:(take' (n - 1) xs)
 
-drop' 0 xs = xs
-drop' _ [] = []
+drop' 0 xs     = xs
+drop' _ []     = []
 drop' n (_:xs) = drop' (n - 1) xs
 
-foldl' f z (x:xs) = f x (foldl' f z xs)
-foldl' _ a _ = a
+foldl' f a (x:xs) = f x (foldl' f a xs)
+foldl' _ a _      = a
 
-concat' xs [] = xs
 concat' lst (x:xs) = concat' (lst ++ [x]) xs
+concat' xs _       = xs
 
 filter' _  [] = []
 filter' f (x:xs) | f x       = x:(filter' f xs)
                  | otherwise = filter' f xs
 
-quickSort' [] = []
+quickSort' []     = []
 quickSort' (x:xs) = concat' (quickSort' (filter' (<= x) xs)) (x:quickSort' (filter' (> x) xs))
