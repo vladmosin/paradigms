c5e5ec7fd49aad9aaf094d65bec8ba37575c0d98
vladmosin <31858377+vladmosin@users.noreply.github.com>
2017-12-09 14:57:04 -0500

    Add files via upload

--- Tree.hs
+++ Tree.hs
@@ -3,22 +3,22 @@ import Prelude hiding (lookup)
 data BinaryTree key value =  Nil | Node key value (BinaryTree key value) (BinaryTree key value) deriving (Show, Eq)
 
 lookup _ Nil = Nothing
-lookup required_key (Node key value left right)
-    | required_key == key = Just value
-    | required_key < key = lookup required_key left
-    | required_key > key = lookup required_key right
+lookup k' (Node k v l r)
+    | k' == k = Just v
+    | k' < k  = lookup k' l
+    | k' > k  = lookup k' r
 
-insert given_key given_value Nil = Node given_key given_value Nil Nil
-insert given_key given_value (Node key value left right)
-    | given_key == key = Node given_key given_value left right
-    | given_key > key = Node key value left (insert given_key given_value right)
-    | given_key < key = Node key value (insert given_key given_value left) right
+insert k' v' Nil = Node k' v' Nil Nil
+insert k' v' (Node k v l r)
+    | k' == k = Node k' v' l r
+    | k' > k  = Node k v l (insert k' v' r)
+    | k' < k  = Node k v (insert k' v' l) r
 
-add_right given_node (Node key value left Nil) = Node key value left given_node
-add_right given_node right = right
+addRight node (Node k v l Nil) = Node k v l node
+addRight node r = r
 
-delete given_key Nil = Nil
-delete given_key (Node key _ left right)
-    | given_key == key = (add_right right left)
-    | given_key < key = delete given_key left
-    | given_key > key = delete given_key right
+delete _ Nil = Nil
+delete k' (Node k v l r)
+    | k' == k = (addRight r l)
+    | k' < k  = Node k v (delete k' l) r
+    | k' > k  = Node k v l (delete k' r)
